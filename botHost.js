c4776b6b8a5630920e0d28a2a885142317a6a1a7
Yuura <63072697+LearningCurveCells@users.noreply.github.com>
2020-04-02 15:38:27 -0700

    Create botHost.js

---
+++ botHost.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+ 
+
+client.on('ready', () => {
+
+    console.log('I am ready!');
+
+});
+
+ 
+
+client.on('message', message => {
+
+    if (message.content === 'ping') {
+
+       message.reply('pong');
+
+       }
+
+});
+
+client.login(process.env.suXx-hB3Ec4JcqIiCUsdwDbjhg9uQvG1);
